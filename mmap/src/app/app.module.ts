325f1787c50d189bf54e98e7effaeb8cfc9b5d7d
Amith Shivantha <amith.shivantha@conxillium.com>
2019-09-03 22:52:10 +0530

    mind-map layout added

---
+++ mmap/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MindMapComponent } from './mind-map/mind-map.component';
+import { NodeDetailComponent } from './node-detail/node-detail.component';
+import { MaterialModule } from './material.module';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MindMapComponent,
+    NodeDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
